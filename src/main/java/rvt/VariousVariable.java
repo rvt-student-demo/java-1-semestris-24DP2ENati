d4ddd4bafdc039879de034651ac079fd6963e632
Edvards <a240584@rvt.lv>
2025-09-10 13:14:57 +0300

    VariouaVariable

---
+++ src/main/java/rvt/VariousVariable.java
@@ -0,0 +1,23 @@
+package rvt;
+
+
+public class VariousVariable {
+
+    public static void main(String[] args) {
+        int chicken = 9000;
+        double bacon_kg = 0.1;
+        String tractor = "Zetor";
+        int summary = 3;
+        System.out.println("chicken:");
+        System.out.println(chicken);
+        System.out.println("bacon_kg");
+        System.out.println(bacon_kg);
+        System.out.println("tractor");
+        System.out.println(tractor);
+        System.out.println("And the summary");
+        System.out.println(summary);
+        
+
+    }
+    
+}
