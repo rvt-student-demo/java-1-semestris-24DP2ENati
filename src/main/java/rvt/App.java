a2f4dc2bf6f1644804246d3f5a5146ea61cc7cbc
Edvards <a240584en@rvt.lv>
2025-10-15 12:54:36 +0300

    App java

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -2,6 +2,13 @@ package rvt;
 
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        int min = 5;
+        int max = 10;
+        musuMetode(min, max);
+        System.out.println(min + " : " + max) ;
+
+    }
+    public static void musuMetode(int min, int max) {
+        min = 1; max = 5;
     }
 }
