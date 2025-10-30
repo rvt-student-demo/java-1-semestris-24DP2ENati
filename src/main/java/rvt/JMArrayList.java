4a99b79ec86a8714a1e3647b900306229d4340ea
Edvards <a240584en@rvt.lv>
2025-10-30 09:06:17 +0200

    Array listi

--- src/main/java/rvt/JMArrayList.java
+++ src/main/java/rvt/JMArrayList.java
@@ -4,15 +4,44 @@ import java.util.*;
 
 public class JMArrayList {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        System.out.println(strings);
+
+        if (!strings.isEmpty()) {
+            strings.remove(strings.size() - 1);
+        }
+
+        System.out.println(strings);
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        if (!strings.isEmpty()) {
+            strings.remove(strings.size() - 1);
+        }
+    }
+
+    public static void listSize() {
         ArrayList<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("")) {
+            if (input.isEmpty()) {
                 break;
             }
             list.add(input);
+        }
+
+        for (String name : list) {
+            System.out.println(name);
         }
 
         System.out.println("In total: " + list.size());
@@ -20,15 +49,53 @@ public class JMArrayList {
         scanner.close();
     }
 
+    public static void onlyTheseNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-    public static void onlyTheseNumbers() {}
-    public static void listSize() {}
-    public static void onTheList(){}
-    public static void removeLast(ArrayList<String> strings) {}
+        while (true) {
+            int num = Integer.parseInt(scanner.nextLine());
+            if (num == -1) {
+                break;
+            }
+            numbers.add(num);
+        }
 
-    
+        System.out.println("From where?");
+        int start = Integer.parseInt(scanner.nextLine());
+        System.out.println("To where?");
+        int end = Integer.parseInt(scanner.nextLine());
 
+        for (int num : numbers) {
+            if (num >= start && num <= end) {
+                System.out.println(num);
 
+                scanner.close();
+            }
+        }
+    }
 
+    public static void onTheList() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            names.add(input);
+        }
+
+        System.out.println("Search for?");
+        String search = scanner.nextLine();
+
+        if (names.contains(search)) {
+            System.out.println(search + " was found!");
+        } else {
+            System.out.println(search + " was not found!");
+
+            scanner.close();
+        }
+    }
 }
